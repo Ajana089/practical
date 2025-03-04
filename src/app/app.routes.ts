d280492d17f07e0355f1be0a692ed8b9f25a8ff0
Ajana Antony <ajanaantony089@gmail.com>
2025-03-04 14:40:49 +0400

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { TestsComponent } from './tests/tests.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',component:TestsComponent
+    },
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+
+    },
+    
+        {
+            path:'**',
+            redirectTo:'home',
+            pathMatch:'full'
+    
+        }
+    
+   
+];
